d47a7c82f79a5c86aa8fdcef64ad3865db707a98
Maxim-Pekov <max.pekov@gmail.com>
2022-07-13 02:06:03 +0300

    Создание админки, предствалий, urls

---
+++ django_movie/movies/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from .models import Movie
+
+
+class MovieView(View):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movies.html', context={'movie_list': movies})
