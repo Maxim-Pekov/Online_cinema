fc809340e45f5b4123316d8bfb80355c9a3ea560
Maxim-Pekov <max.pekov@gmail.com>
2022-07-14 02:28:44 +0300

    add detail_view

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
@@ -9,16 +9,19 @@ from .models import Movie
 #
 #     def get(self, request):
 #         movies = Movie.objects.all()
-#         return render(request, 'movies/movies.html', context={'movie_list': movies})
+#         return render(request, 'movies/movie_list.html', context={'movie_list': movies})
 
 class MovieView(ListView):
     # paginate_by = 2
     model = Movie
-    template_name = 'movies/movies.html'
     queryset = Movie.objects.all()
 
 
-class MovieDetailView(View):
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/movie_detail.html', context={'movie': movie})
+# class MovieDetailView(View):
+#     def get(self, request, slug):
+#         movie = Movie.objects.get(url=slug)
+#         return render(request, 'movies/movie_detail.html', context={'movie': movie})
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
